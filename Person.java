cec9c175869caf9d6b265d6ac9343d5e8a3a84bf
Za <salma.azizah@amikom.ac.id>
2018-12-03 16:33:20 +0700

    Penambahan file Hello.java dan Person.java

---
+++ Person.java
@@ -0,0 +1,11 @@
+package bank.customers;
+
+public class Person {
+
+	String nama;
+	@Override
+	public String toString() {
+		return "Person [nama=" + nama + ", noKtp=" + noKtp + "]";
+	}
+	String noKtp;
+}
